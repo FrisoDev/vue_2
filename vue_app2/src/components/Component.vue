b7b2e01e8e00588c901fc168dc51c5e20ef08867
FrisoDev <frisodevstoker@gmail.com>
2018-01-17 17:16:27 +0100

    Module Not Found

---
+++ vue_app2/src/components/Component.vue
@@ -0,0 +1,9 @@
+<script>
+    export default {
+        beforeCreate () {
+            if (!store.state.isLogged) {
+                router.push('/login')
+            }
+        }
+    }
+</script>
